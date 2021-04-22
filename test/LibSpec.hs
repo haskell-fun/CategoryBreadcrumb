59f802efdbadc1fbd12ca9b0cea5c3990e6ca3e6
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-04-22 23:14:53 +0100

    Provide gen and arbitrary instances for catId and cat

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,26 +1,41 @@
 module LibSpec where
 
-import Data.Maybe ( isNothing )
--- import Test.QuickCheck.Arbitrary ( Arbitrary )
-import Test.Hspec ( describe, it, shouldBe, Spec )
-import Test.Hspec.QuickCheck ( prop )
-import Test.QuickCheck.Property ( (==>) )
-import Lib ( breadcrumb, Category(categoryId), CategoryId, CategoryTree )
+import Data.Maybe (isNothing)
+import Lib
+import Test.Hspec (Spec, describe)
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck (Gen, listOf)
+import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
+import Test.QuickCheck.Property (forAll)
 
--- instance Arbitrary CategoryId
--- instance Arbitrary Category
+genCategoryId :: Gen CategoryId
+genCategoryId = CategoryId <$> arbitrary
+
+instance Arbitrary CategoryId where
+  arbitrary = genCategoryId
+
+-- this is a silly generator, we want to generate something better next time + avoid orphan instances
+instance Arbitrary Category where
+  arbitrary = sillyGenCategory
+
+sillyGenCategory :: Gen Category
+sillyGenCategory = Category <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
 
 spec :: Spec
 spec = describe "breadcrumb" $ do
+  prop "returns Nothing iff categoryId is not present in the tree (manual monadic generators)" $
+    forAll genCategoryId $ \c ->
+      forAll (listOf sillyGenCategory) $ \t ->
+        let isCategoryPresent t cid = cid `elem` fmap categoryId t
+         in isNothing (breadcrumb t c) == not (isCategoryPresent t c)
 
-   prop "returns Nothing iff categoryId is not present in the tree" $
-     \t c -> isNothing (breadcrumb t c) == not (isCategoryPresent t c)
+  prop "returns Nothing iff categoryId is not present in the tree (automatic type class resolution)" $
+    \c t ->
+      let isCategoryPresent t c = c `elem` fmap categoryId t
+       in isNothing (breadcrumb t c) == not (isCategoryPresent t c)
 
-   -- prop "must return Nothing if categoryId is not present in the tree" $
-   --   \t c -> not (isCategoryPresent t c) ==> isNothing (breadcrumb t c)
+-- prop "must return Nothing if categoryId is not present in the tree" $
+--   \t c -> not (isCategoryPresent t c) ==> isNothing (breadcrumb t c)
 
-   -- prop "must return Nothing ONLY if categoryId is not present in the tree" $
-   --   \t c -> isNothing (breadcrumb t c) ==> not (isCategoryPresent t c)
-
-isCategoryPresent :: CategoryTree -> CategoryId -> Bool
-isCategoryPresent t cid = cid `elem` fmap categoryId t
+-- prop "must return Nothing ONLY if categoryId is not present in the tree" $
+--   \t c -> isNothing (breadcrumb t c) ==> not (isCategoryPresent t c)
