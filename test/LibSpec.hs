ce2a63ecff84fa885f5ab85d53d0ae062df8c4c0
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-04-29 00:53:39 +0100

    Aeson custom deserialiser

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -4,12 +4,21 @@ import Data.Maybe (isNothing)
 import Lib
 import Test.Hspec (Spec, describe)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (Gen, listOf)
+import Test.QuickCheck (Gen, elements, listOf)
 import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
 import Test.QuickCheck.Property (forAll)
 
 genCategoryId :: Gen CategoryId
 genCategoryId = CategoryId <$> arbitrary
+
+instance Arbitrary CategoryState where
+  arbitrary = elements [Active, Inactive]
+
+instance Arbitrary CategorySelectableState where
+  arbitrary = elements [Selectable, Unselectable]
+
+instance Arbitrary CategoryDescription where
+  arbitrary = CategoryDescription <$> arbitrary
 
 instance Arbitrary CategoryId where
   arbitrary = genCategoryId
